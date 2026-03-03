1d9f517d4bdd9f3e9ca8b2099042ee41259c5b54
Quang Anh <anhdq2109@gmail.com>
2026-03-03 16:11:09 +0700

    Add question-answer models and update DB context

--- Backend/Models/StudentAnswer.cs
+++ Backend/Models/StudentAnswer.cs
@@ -5,15 +5,17 @@ namespace Backend.Models;
 
 public partial class StudentAnswer
 {
-    public int AnsId { get; set; }
+    public int StudentAnswersId { get; set; }
 
     public int SubmissionId { get; set; }
 
-    public int QuestionIndex { get; set; }
+    public int QuestionAnswersId { get; set; }
 
-    public string? ResponseText { get; set; }
+    public string? Response { get; set; }
 
     public byte[] ConcurrencyStamp { get; set; } = null!;
+
+    public virtual QuestionAnswer QuestionAnswers { get; set; } = null!;
 
     public virtual Submission Submission { get; set; } = null!;
 }
