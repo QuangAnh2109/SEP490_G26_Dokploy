58bc26aeb1fb23bfb030b44e7cfff152e541839d
Quang Anh <anhdq2109@gmail.com>
2026-03-06 03:46:30 +0700

    Make Content nullable & render MCQ dynamically

--- Backend/Services/Implements/StudentExamService.cs
+++ Backend/Services/Implements/StudentExamService.cs
@@ -95,7 +95,7 @@ namespace Backend.Services.Implements
                 var answers = q.QuestionAnswers.Select(qa => new TakeExamAnswerDto
                 {
                     QuestionAnswerId = SecureIdHelper.EncryptId(qa.QuestionAnswerId),
-                    Content = qa.Content,
+                    Content = q.QuestionType == QuestionType.Mcq ? qa.Content : null,
                     GroupAnswerId = qa.GroupAnswerId.HasValue
                         ? SecureIdHelper.EncryptId(qa.GroupAnswerId.Value)
                         : null,
